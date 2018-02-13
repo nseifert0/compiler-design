62a8f91884d91c0b3fcda7bfa7490fee1819761b
NMSeifert <nicholas.m.seifert@gmail.com>
2018-02-12 22:44:20 -0500

    Added basic constructor and methods to Lexer class.

--- Lexer/lexer.hpp
+++ Lexer/lexer.hpp
@@ -1,8 +1,21 @@
 #ifndef LEXER_HPP
 #define LEXER_HPP
 
+#include <fstream>
+
 class Lexer {
+	public:
+		Lexer(const char* path)
+			: file(path)
+		{
+		}
 	
+	void lex();
+	bool eof();
+	char peek();
+	
+	private:
+		std::ifstream file;
 };
 
 #endif
--- Lexer/main.cpp
+++ Lexer/main.cpp
@@ -1,26 +1,9 @@
 #include <iostream>
-#include <fstream>
-#include <string>
 
 #include "lexer.hpp"
 
 int main() {
-	std::ifstream file ("Input.txt");
-	
-	char c;
-	char d;
-	
-	if (file.is_open())
-	{
-		while (!file.eof())
-		{
-			std::cout << file.eof() << "\n";
-			c = file.get();
-			std::cout << c << "\n";
-			std::cout << file.peek() << "\n";
-			std::cout << file.eof() << "\n";
-		}
-    file.close();
-  }
+	Lexer L("Input.txt");	
+	std::cout << "Hello World\n";
 	return 0;
 }
