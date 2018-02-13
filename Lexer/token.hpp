1ef9d65af7ded70f1c215f70afd21475927ef21b
NMSeifert <nicholas.m.seifert@gmail.com>
2018-02-13 12:29:04 -0500

    Added an enum for the token types.

--- Lexer/token.hpp
+++ Lexer/token.hpp
@@ -1,6 +1,46 @@
 #ifndef TOKEN_HPP
 #define TOKEN_HPP
 
+enum TokType {
+	LeftBrace,
+	RightBrace,
+	LeftParen,
+	RightParen,
+	LeftBracket,
+	RightBracket,
+	Comma,
+	Semicolon,
+	Colon,
+	RelationalOp,
+	ArithmeticOp,
+	BitwiseOp,
+	LogicalOp,
+	ConditionalOp,
+	AssignmentOp,
+	KeywAnd,
+	KeywBool,
+	KeywChar,
+	KeywDef,
+	KeywElse,
+	KeywFalse,
+	KeywFloat,
+	KeywIf,
+	KeywInt,
+	KeywLet,
+	KeywNot,
+	KeywOr,
+	KeywTrue,
+	KeywVar,
+	Identifier,
+	DecimalIntegerLiteral,
+	HexadecimalIntegerLiteral,
+	BinaryIntegerLiteral,
+	FloatingPointLiteral,
+	BooleanLiteral,
+	CharacterLiteral,
+	StringLiteral
+};
+
 class Token {
 	
 	public:
