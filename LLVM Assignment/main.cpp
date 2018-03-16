12e41036026446d4e181033da1e08887e358be0c
NMSeifert <nicholas.m.seifert@gmail.com>
2018-03-16 09:49:33 -0400

    Took another step in implementation.

--- LLVM Assignment/main.cpp
+++ LLVM Assignment/main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 
-extern "C" int string_len(int x);
+extern "C" int string_len(const char* str);
 //extern "C" int string_cmp(const char* str1, const char* str2);
 //extern "C" const char* string_chr(const char* str, char c);
 //extern "C" char* string_cpy(char* dest, const char* src);
 
 int main() {
-  int bunga = string_len(10);
-  std::cout << bunga << "\n";
+  char* s = "Hi";
+  std::cout << string_len(s) << "\n";
 }
--- LLVM Assignment/string.ll
+++ LLVM Assignment/string.ll
@@ -1,3 +1,4 @@
-define i32 @string_len(i32 %x) {
+define i32 @string_len(i8 * %str) {
+	%x = add i32 1, 0
 	ret i32 %x
 }
