9741e85dd288c7389fc8fc34ef6f4f889084896c
NMSeifert <nicholas.m.seifert@gmail.com>
2018-03-15 20:33:12 -0400

    Added files for LLVM assignment.

---
+++ LLVM Assignment/main.cpp
@@ -0,0 +1,8 @@
+extern "C" int string_len(const char* str);
+extern "C" int string_cmp(const char* str1, const char* str2);
+extern "C" const char* string_chr(const char* str, char c);
+extern "C" char* string_cpy(char* dest, const char* src);
+
+int main() {
+  // Test code
+}
