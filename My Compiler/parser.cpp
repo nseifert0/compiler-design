f6dd0e0bad1e3263cd59a800468475c6b23505a8
NMSeifert <nicholas.m.seifert@gmail.com>
2018-03-18 19:13:52 -0400

    Added parser skeleton with function definitions and declarations.

--- My Compiler/parser.cpp
+++ My Compiler/parser.cpp
@@ -3,6 +3,168 @@
 void Parser::parse() {
 }
 
+//------------------------------------------------------------------------------
+//Parsing Types
+void Parser::parseBasicType() {
+}
+
+void Parser::parseTypeList() {
+}
+
+void Parser::parsePostfixType() {
+}
+
+void Parser::parseReferenceType() {
+}
+
+void Parser::parseType() {
+}
+
+//------------------------------------------------------------------------------
+//Parsing Expressions
+void Parser::parsePrimaryExpression() {
+}
+
+void Parser::parsePostfixExpression() {
+}
+
+void Parser::parseArgumentList() {
+}
+
+void Parser::parseArgument() {
+}
+
+void Parser::parseUnaryExpression() {
+}
+
+void Parser::parseCastExpression() {
+}
+
+void Parser::parseMultiplicativeExpression() {
+}
+
+void Parser::parseAdditiveExpression() {
+}
+
+void Parser::parseShiftExpression() {
+}
+
+void Parser::parseRelationalExpression() {
+}
+
+void Parser::parseEqualityExpression() {
+}
+
+void Parser::parseBitwiseAndExpression() {
+}
+
+void Parser::parseBitwiseXorExpression() {
+}
+
+void Parser::parseBitwiseOrExpression() {
+}
+
+void Parser::parseLogicalAndExpression() {
+}
+
+void Parser::parseLogicalOrExpression() {
+}
+
+void Parser::parseConditionalExpression() {
+}
+
+void Parser::parseAssignmentExpression() {
+}
+
+void Parser::parseExpression() {
+}
+
+void Parser::parseConstantExpression() {
+}
+
+
+//------------------------------------------------------------------------------
+//Parsing Statements
+void Parser::parseStatement() {
+}
+
+void Parser::parseBlockStatement() {
+}
+
+void Parser::parseStatementSeq() {
+}
+
+void Parser::parseIfStatement() {
+}
+
+void Parser::parseWhileStatement() {
+}
+
+void Parser::parseBreakStatement() {
+}
+
+void Parser::parseContinueStatement() {
+}
+
+void Parser::parseReturnStatement() {
+}
+
+void Parser::parseDeclarationStatement() {
+}
+
+void Parser::parseExpressionStatement() {
+}
+
+//------------------------------------------------------------------------------
+//Parsing Declarations
+void Parser::parseProgram() {
+}
+
+void Parser::parseDeclarationSeq() {
+}
+
+void Parser::parseDeclaration() {
+}
+
+void Parser::parseLocalDeclaration() {
+}
+
+void Parser::parseObjectDefinition() {
+}
+
+void Parser::parseVariableDefinition() {
+}
+
+void Parser::parseConstantDefinition() {
+}
+
+void Parser::parseValueDefinition() {
+}
+
+void Parser::parseFunctionDefinition() {
+}
+
+void Parser::parseParameterList() {
+}
+
+void Parser::parseParameter() {
+}
+
+//------------------------------------------------------------------------------
+//Parser Utilities
 void Parser::pushToken() {
 	tokenQueue.push_back(lexer.lex());
+}
+
+Token Parser::lookAhead(int howMuch) {
+	while(tokenQueue.size() < howMuch + 1) {
+		pushToken();
+	}
+	return tokenQueue[howMuch];
+}
+
+Token Parser::accept() {
+	Token acceptedToken = lookAhead(0);
+	tokenQueue.pop_front();
+	return acceptedToken;
 }
--- My Compiler/parser.hpp
+++ My Compiler/parser.hpp
@@ -10,14 +10,73 @@
 
 class Parser {
 	public:
-		Parser(const char* filepath):lexer(filepath) {
+		Parser(const char* filepath): lexer(filepath) {
 		}
 		
-		void pushToken();
 		void parse();
+		
+		//Parsing Types
+		void parseBasicType();
+		void parseTypeList();
+		void parsePostfixType();
+		void parseReferenceType();
+		void parseType();
+		
+		//Parsing Expressions
+		void parsePrimaryExpression();
+		void parsePostfixExpression();
+		void parseArgumentList();
+		void parseArgument();
+		void parseUnaryExpression();
+		void parseCastExpression();
+		void parseMultiplicativeExpression();
+		void parseAdditiveExpression();
+		void parseShiftExpression();
+		void parseRelationalExpression();
+		void parseEqualityExpression();
+		void parseBitwiseAndExpression();
+		void parseBitwiseXorExpression();
+		void parseBitwiseOrExpression();
+		void parseLogicalAndExpression();
+		void parseLogicalOrExpression();
+		void parseConditionalExpression();
+		void parseAssignmentExpression();
+		void parseExpression();
+		void parseConstantExpression();
+	
+		//Parsing Statements
+		void parseStatement();
+		void parseBlockStatement();
+		void parseStatementSeq();
+		void parseIfStatement();
+		void parseWhileStatement();
+		void parseBreakStatement();
+		void parseContinueStatement();
+		void parseReturnStatement();
+		void parseDeclarationStatement();
+		void parseExpressionStatement();
+		
+		//Parsing Declarations
+		void parseProgram();
+		void parseDeclarationSeq();
+		void parseDeclaration();
+		void parseLocalDeclaration();
+		void parseObjectDefinition();
+		void parseVariableDefinition();
+		void parseConstantDefinition();
+		void parseValueDefinition();
+		void parseFunctionDefinition();
+		void parseParameterList();
+		void parseParameter();
+	
 	private:
 		Lexer lexer;
 		std::deque<Token> tokenQueue;
+		
+		//Parser Utilities
+		void pushToken();
+		Token lookAhead(int howMuch);
+		Token accept();
 };
 
 #endif
