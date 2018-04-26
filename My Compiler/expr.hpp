cc0fd4dfb5687c03234426d31265897d714a4f63
NMSeifert <nicholas.m.seifert@gmail.com>
2018-04-26 03:15:37 -0400

    Added many enums

--- My Compiler/expr.hpp
+++ My Compiler/expr.hpp
@@ -6,24 +6,71 @@
 
 enum whatExpr {
 	exprIsTest,
-	/*primaryExpression;
-	postfixExpression;
-	argumentList;
-	unaryExpression;
-	castExpression;
-	multiplicativeExpression;
-	additiveExpression;
-	shiftExpression;
-	relationalExpression;
-	equalityExpression;
-	bitwiseAndExpression;
-	bitwiseXorExpression;
-	bitwiseOrExpression;
-	logicalAndExpression;
-	logicalOrExpression;
-	conditionalExpression;
-	assignmentExpression;
-	*/
+	primaryExpression,
+	postfixExpression,
+	argumentList,
+	unaryExpression,
+	castExpression,
+	multiplicativeExpression,
+	additiveExpression,
+	shiftExpression,
+	relationalExpression,
+	equalityExpression,
+	bitwiseAndExpression,
+	bitwiseXorExpression,
+	bitwiseOrExpression,
+	logicalAndExpression,
+	logicalOrExpression,
+	conditionalExpression,
+	assignmentExpression,
+};
+
+enum whatPrimaryExpr {
+	literal,
+	identifier,
+	parenthesizedPrim
+};
+
+enum whatPostfixExpr {
+	parenthesizedPost,
+	bracketed
+};
+
+enum whatUnaryExpr {
+	positive,
+	negative,
+	complement,
+	opposite,
+	address,
+	dereference,
+};
+
+enum whatMultiplicativeExpr {
+	multiply,
+	divide,
+	modulo
+};
+
+enum whatAdditiveExpr {
+	add,
+	subtract
+};
+
+enum whatShiftExpr {
+	left,
+	right
+};
+
+enum whatRelationalExpr {
+	lessThan,
+	greaterThan,
+	lessThanOrEqual,
+	greaterThanOrEqual
+};
+
+enum whatEqualityExpr {
+	equal,
+	notEqual
 };
 
 class Expr {
